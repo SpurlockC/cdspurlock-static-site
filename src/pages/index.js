a46073f1c85658f1514d00c7ca1f1362b01f2c0d
SpurlockC <evilskullygaming@gmail.com>
2024-10-09 08:57:33 -0500

    updating static site to have a blog API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
-import React from 'react'
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
-export default function Home() {
-  return <div>Hello, Gatsby!</div>
-}
+const posts = [
+    { id: 1, title: 'Best Practices for Using APIs in React', content: 'Lorem ipsum...' },
+    { id: 2, title: 'Building an API with Express', content: 'Lorem ipsum...' },
+    { id: 3, title: 'Deploying APIs with Netlify Functions', content: 'Lorem ipsum...' },
+];
+
+app.get('/api/posts', (req, res) => {
+    res.json(posts);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
